6fdf8906360fc6b5977720119fc13eb90bc03e9e
Akshay-Daundkar-14 <akshaydaundkar2000@gmail.com>
2024-02-21 19:46:01 +0530

    Test

---
+++ VehicleServiceManagement.API/Controllers/DummyController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleServiceManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DummyController : ControllerBase
+    {
+    }
+}
