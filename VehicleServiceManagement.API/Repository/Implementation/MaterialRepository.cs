9560ac73c52338117e9601cf83d6b31f176fda91
Akshay-Daundkar-14 <akshaydaundkar2000@gmail.com>
2024-02-23 12:27:09 +0530

    Material CRUD with repository and soft delete

---
+++ VehicleServiceManagement.API/Repository/Implementation/MaterialRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleServiceManagement.API.Data;
+using VehicleServiceManagement.API.Models.Domain;
+using VehicleServiceManagement.API.Repository.Interface;
+
+namespace VehicleServiceManagement.API.Repository.Implementation
+{
+    public class MaterialRepository : IMaterialRepository
+    {
+        private readonly ApplicationDbContext _appDbContext;
+
+        public MaterialRepository(ApplicationDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task CreateMaterialAsync(Material material)
+        {
+            await _appDbContext.Materials.AddAsync(material);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteMaterialAsync(Material material)
+        {
+            material.IsDeleted = true;
+            _appDbContext.Entry(material).State = EntityState.Modified;
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Material>> GetAllMaterialAsync()
+        {
+            var result = await _appDbContext.Materials.ToListAsync();
+            return result;
+        }
+
+        public async Task<Material> GetMaterialAsync(int id)
+        {
+            var material = await _appDbContext.Materials.FindAsync(id);
+            return material;
+        }
+
+        public async Task UpdateMaterialAsync(Material material)
+        {
+            _appDbContext.Entry(material).State = EntityState.Modified;
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ VehicleServiceManagement.API/Repository/Interface/IMaterialRepository.cs
@@ -0,0 +1,19 @@
+﻿using VehicleServiceManagement.API.Models.Domain;
+
+namespace VehicleServiceManagement.API.Repository.Interface
+{
+    public interface IMaterialRepository
+    {
+        public Task<List<Material>> GetAllMaterialAsync();
+
+        public Task<Material> GetMaterialAsync(int id);
+
+
+        public Task CreateMaterialAsync(Material material);
+
+        public Task UpdateMaterialAsync(Material material);
+
+
+        public Task DeleteMaterialAsync(Material material);
+    }
+}
